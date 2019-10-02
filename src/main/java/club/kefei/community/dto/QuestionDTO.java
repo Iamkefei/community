c51e2e673c2ba8b78288106041aa5b321b78c601
zhu <928385924@qq.com>
2019-10-02 22:27:30 +0800

    渲染列表1

---
+++ src/main/java/club/kefei/community/dto/QuestionDTO.java
@@ -0,0 +1,19 @@
+package club.kefei.community.dto;
+
+import club.kefei.community.modal.User;
+import lombok.Data;
+
+@Data
+public class QuestionDTO {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer creator;
+    private Integer viewCount;
+    private Integer commentCount;
+    private Integer likeCount;
+    private User user;
+}
