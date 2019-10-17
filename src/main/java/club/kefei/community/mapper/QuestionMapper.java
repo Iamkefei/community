7e443f946e7d11c2cd76eaabe974399746e817a0
zhu <928385924@qq.com>
2019-10-17 22:00:52 +0800

    分页2233

--- src/main/java/club/kefei/community/mapper/QuestionMapper.java
+++ src/main/java/club/kefei/community/mapper/QuestionMapper.java
@@ -15,13 +15,16 @@ public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
 
-    @Select("select * from question limit #{offset},${size}")
+    @Select("select * from question order by id limit #{offset},${size}")
     List<Question> list(Integer offset, Integer size);
 
     @Select("select count(1) from question")
     Integer count();
 
 
-    @Select("select * from question where creator = #{userId} limit #{offset},${size}")
-    List<Question> list(Integer userId, Integer offset, Integer size);
+    @Select("select * from question where creator = #{userId} order by id limit #{offset},${size}")
+    List<Question> listByUserId(Integer userId, Integer offset, Integer size);
+
+    @Select("select count(1) from question where creator = #{userId}")
+    Integer countByUserId(Integer userId);
 }
