41e8a2a902a9acb3c2d0f2ab16cd0550765da527
zhu <928385924@qq.com>
2019-10-20 20:27:07 +0800

    拦截器

--- src/main/java/club/kefei/community/controller/PublishController.java
+++ src/main/java/club/kefei/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package club.kefei.community.controller;
 
 import club.kefei.community.mapper.QuestionMapper;
-import club.kefei.community.mapper.UserMapper;
 import club.kefei.community.modal.Question;
 import club.kefei.community.modal.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @Autowired
     private QuestionMapper questionMapper;
-
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish() {
@@ -51,20 +46,7 @@ public class PublishController {
         model.addAttribute("description", description);
         model.addAttribute("tag", tag);
 
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if(cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if(user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
 
         if(user == null) {
             model.addAttribute("error", "用户未登录");
