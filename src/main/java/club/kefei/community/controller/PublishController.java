985d4d43139ae223fd0589b874dc236c562b3ed4
zhu <928385924@qq.com>
2019-10-24 21:56:08 +0800

    编辑功能

--- src/main/java/club/kefei/community/controller/PublishController.java
+++ src/main/java/club/kefei/community/controller/PublishController.java
@@ -3,10 +3,12 @@ package club.kefei.community.controller;
 import club.kefei.community.mapper.QuestionMapper;
 import club.kefei.community.modal.Question;
 import club.kefei.community.modal.User;
+import club.kefei.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +18,20 @@ import javax.servlet.http.HttpServletRequest;
 public class PublishController {
     @Autowired
     private QuestionMapper questionMapper;
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Integer id,
+                       Model model) {
+        Question question = questionMapper.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id", question.getId());
+        return "publish";
+    }
 
     @GetMapping("/publish")
     public String publish() {
@@ -27,6 +43,7 @@ public class PublishController {
             @RequestParam("title") String title,
             @RequestParam("description") String description,
             @RequestParam("tag") String tag,
+            @RequestParam(value = "id", required = false) Integer id,
             HttpServletRequest request,
             Model model) {
         if(title == null || title == "") {
@@ -58,10 +75,9 @@ public class PublishController {
         question.setDescription(description);
         question.setTag(tag);
         question.setCreator(user.getId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
-
-        questionMapper.create(question);
+        question.setId(id);
+        questionService.createOrUpdate(question);
+//        questionMapper.create(question);
         return "redirect:/";
     }
 }
