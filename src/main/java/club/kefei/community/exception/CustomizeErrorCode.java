bc0a6154859dddc6fc8eea21c4d0d7409b4d3666
zhu <928385924@qq.com>
2019-10-30 21:03:30 +0800

    换个视频

--- src/main/java/club/kefei/community/exception/CustomizeErrorCode.java
+++ src/main/java/club/kefei/community/exception/CustomizeErrorCode.java
@@ -4,7 +4,9 @@ public enum  CustomizeErrorCode implements ICustomizeErrorCode {
     QUESTION_NOT_FOUND(2001, "你找到问题不在了,要不要换个试试？"),
     TARGET_PARAM_NOT_FOUND(2001, "为选中任何问题或者评论进行回复"),
     NO_LOGIN(2003, "未登录不能进行评论， 请先登录"),
-    SYS_ERROR(2004, "服务器冒烟了, 要不你稍后再试试")
+    SYS_ERROR(2004, "服务器冒烟了, 要不你稍后再试试"),
+    TYPE_PARAM_WRONG(2005, "评论类型错误或不存在"),
+    COMMENT_NOT_FOUND(2006, "你找到评论不存在,要不要换个试试？");
     ;
 
     @Override
--- src/main/java/club/kefei/community/service/CommentService.java
+++ src/main/java/club/kefei/community/service/CommentService.java
@@ -1,15 +1,41 @@
 package club.kefei.community.service;
 
+import club.kefei.community.enums.CommentTypeEnum;
 import club.kefei.community.exception.CustomizeErrorCode;
 import club.kefei.community.exception.CustomizeException;
+import club.kefei.community.mapper.CommentMapper;
+import club.kefei.community.mapper.QuestionMapper;
 import club.kefei.community.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
     public void insert(Comment comment) {
         if(comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
+        }
+
+        if(comment.getType() == null || CommentTypeEnum.isExist(comment.getType())) {
+            throw new CustomizeException(CustomizeErrorCode.TYPE_PARAM_WRONG);
+        }
+
+        if(comment.getType() == CommentTypeEnum.COMMENT.getType()) {
+            // 回复评论
+            Comment dbComment = commentMapper.selectByPrimaryKey(comment.getParentId());
+            if(dbComment == null) {
+                throw new CustomizeException(CustomizeErrorCode.COMMENT_NOT_FOUND);
+            }
+            commentMapper.insert(comment);
+        }else {
+            // 回复问题
+
         }
     }
 }
